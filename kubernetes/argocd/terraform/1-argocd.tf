19a0329235b292016d61450a6b9025b45ad274d4
Ittipol Kaewprasert <Ittipol.Kaewprasert@krungsrinimble.com>
2025-02-22 16:08:13 +0700

    Update config

--- kubernetes/argocd/terraform/1-argocd.tf
+++ kubernetes/argocd/terraform/1-argocd.tf
@@ -1,14 +1,14 @@
 # Install manually
 # helm repo add argo https://argoproj.github.io/argo-helm
 # helm repo update
-# helm install argocd --namespace argocd --create-namespace argo/argo-cd --version 7.7.13
+# helm install argocd --namespace argocd --create-namespace argo/argo-cd --version 7.8.3
 resource "helm_release" "argocd" {
   name = "argocd"
 
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
   namespace        = "argocd"
-  version          = "7.7.13"
+  version          = "7.8.3"
   create_namespace = true
 
   values = [file("values/argocd_value.yaml")]
