26a51a54066163ca27077c7add4f918e8a7b28b7
Ittipol Kaewprasert <Ittipol.Kaewprasert@krungsrinimble.com>
2025-03-18 00:39:14 +0700

    Update config

--- kubernetes/prometheus/terraform/1-prometheus.tf
+++ kubernetes/prometheus/terraform/1-prometheus.tf
@@ -1,14 +1,14 @@
 # Install manually
 # helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 # helm repo update
-# helm install prometheus --namespace prometheus --create-namespace --version 27.4.0 --values kubernetes/terraform/values/prometheus-helm.yaml prometheus-community/prometheus
+# helm install prometheus --namespace prometheus --create-namespace --version 27.5.1 --values kubernetes/terraform/values/prometheus-helm.yaml prometheus-community/prometheus
 resource "helm_release" "prometheus" {
   name = "prometheus"
 
   repository       = "https://prometheus-community.github.io/helm-charts"
   chart            = "prometheus"
   namespace        = "prometheus"
-  version          = "27.4.0"
+  version          = "27.5.1"
   create_namespace = true
 
   values = [file("values/prometheus-value.yaml")]
